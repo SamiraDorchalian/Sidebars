25de2a18552c539da32f6834475054c73d0a2f40
samira dorchalian <samiradorchalain1998@gmail.com>
2023-08-26 19:50:28 +0430

    Testimonial Slider Design Step by Step

---
+++ Testimonial Slider Design Step by Step/script.js
@@ -0,0 +1,22 @@
+var slide = document.getElementById("slide");
+var upArrow = document.getElementById("upArrow");
+var downArrow = document.getElementById("downArrow");
+
+
+let x = 0;
+
+upArrow.onclick = function(){
+    if(x > "-1700"){
+        x = x - 300;
+        slide.style.top = x + "px";
+    }
+    
+}
+
+downArrow.onclick = function(){
+    if(x < 0){
+        x = x + 300;
+        slide.style.top = x + "px";
+    }
+    
+}
