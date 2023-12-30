3806314c51012025833550eb850ab653dda38933
samira dorchalian <samiradorchalain1998@gmail.com>
2023-12-30 15:27:18 +0330

    Custom Select Box Design

--- Custom Select Box Design/script.js
+++ Custom Select Box Design/script.js
@@ -5,15 +5,28 @@ var options = document.getElementsByClassName("options");
 var list = document.getElementById("list");
 var arrowIcon = document.getElementById("arrowIcon");
 
-selectField.onclick = function(){
-    list.classList.toggle("hide");
-    arrowIcon.classList.toggle("rotate");
-}
+// selectField.onclick = function(){
+//     list.classList.toggle("hide");
+//     arrowIcon.classList.toggle("rotate");
+// }
+const selectFieldHandler = () => {
+  list.classList.toggle("hide");
+  arrowIcon.classList.toggle("rotate");
+};
+selectField.addEventListener("click", selectFieldHandler);
 
-for(option of options){
-    option.onclick = function(){
-        selectText.innerHTML = this.textContent;
-        list.classList.toggle("hide");
-        arrowIcon.classList.toggle("rotate");
-    }
+// for (option of options) {
+//   option.onclick = () => {
+//     selectText.innerHTML = this.textContent;
+//     list.classList.toggle("hide");
+//     arrowIcon.classList.toggle("rotate");
+//   };
+// }
+for (option of options) {
+  option.addEventListener("click", options);
 }
+const optionsHandler = () => {
+  selectText.innerHTML = this.textContent;
+  list.classList.toggle("hide");
+  arrowIcon.classList.toggle("rotate");
+};
